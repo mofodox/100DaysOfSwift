64cd57191f50ee47f19cd8c621e3979893e67eb5
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-07-29 11:35:01 +0800

    add arithmetic operations

---
+++ Day3/arithmetic-operations.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Operators are those mathematical symbols like + and -, and Swift has a huge range of them
+
+let firstScore = 12
+let secondScore = 4
+
+// addition and substraction
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+// multiplication and division
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+// Swift has a special operator for calculating remainders after division: %
+let remainder = 13 % secondScore
